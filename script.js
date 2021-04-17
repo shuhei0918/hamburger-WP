8f4dfbca7ead9879cd8a27980bb9bb71379a37a1
shuhei0918 <pusyukeyama@gmail.com>
2021-04-17 21:35:29 +0900

    =edit

--- script.js
+++ script.js
@@ -5,15 +5,28 @@ $(function(){
         var close = $('#c-menu__close').prop('checked');
 
         if(open){
-            $('.l-sidebar').toggleClass("is-open")
-            $('.l-header').toggleClass("is-open")
-            $('.l-main').toggleClass("is-open")
-            $('.l-footer').toggleClass("is-open")
-        }else{
-            $('.l-sidebar').removeClass("is-open")
-            $('.l-header').removeClass("is-open")
-            $('.l-main').removeClass("is-open")
-            $('.l-footer').removeClass("is-open")
+            $('.l-sidebar').toggleClass("is-open");
+            $('.l-header').toggleClass("is-open");
+            $('.l-main').toggleClass("is-open");
+            $('.l-footer').toggleClass("is-open");
+
+        }else if(close){
+            $('.l-sidebar').removeClass("is-open");
+            $('.l-header').removeClass("is-open");
+            $('.l-main').removeClass("is-open");
+            $('.l-footer').removeClass("is-open");
+
+        }
+    });
+
+    $(window).resize(function(){
+        var open = $('#c-menu__open').prop('checked');
+        if(open){
+            $('.l-sidebar').removeClass("is-open");
+            $('.l-header').removeClass("is-open");
+            $('.l-main').removeClass("is-open");
+            $('.l-footer').removeClass("is-open");
+            $('#c-menu__close').prop('checked',true);
         }
     });
 });
