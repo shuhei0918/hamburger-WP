6662d2d4c7dd5a93609e918e14cdeb0e9b64b5fe
shuhei0918 <pusyukeyama@gmail.com>
2021-07-01 05:17:25 +0900

    edit

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+        <main class="l-main">
+            <section class="p-top__index p-top">
+                <img src="<?php header_image(); ?>">
+                <h2>ダミーサイト</h2>
+            </section>
+            <article class="p-article__404 p-article">
+                <h2>404 Not Found</h2>
+            </article>
+        </main>
+    </div>
+    <?php get_sidebar(); ?>
+    <?php get_footer(); ?>
